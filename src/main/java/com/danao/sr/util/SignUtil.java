ab04c4d9aeb1ed6819a25f84e0d0d93bde79b1a2
zhengyanjunmac <11023754@qq.com>
2021-03-22 20:48:59 +0800

    提交测试微信

--- src/main/java/com/danao/sr/util/SignUtil.java
+++ src/main/java/com/danao/sr/util/SignUtil.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class SignUtil {
     // 与接口配置信息中的Token要一致
-    private static String token = "dangao";
+    private static String token = "DaoKeC4Good";
 
     /**
      * 验证签名
